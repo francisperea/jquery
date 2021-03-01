e5d8161f45c457579e3069ac999b16df4ea6ee15
Francis Perea <francis@francisperea.org>
2021-03-01 21:13:42 +0000

    Muestra y oculta contenido

--- script4.js
+++ script4.js
@@ -1,6 +1,9 @@
-$(document).ready(function() {
-  var cajaoculta = $( "#oculta" );
-$( "#encendedor" ).on( "click", function( event ) {
-  cajaoculta.show("slow");
-});
+$(document).ready(function () {
+  var cajaoculta = $("#oculta");
+  $("#encendedor").on("click", function (event) {
+    cajaoculta.show(1000, "swing");
+  });
+  $("#ocultador").on("click", function (event) {
+    cajaoculta.hide(1000, "swing");
+  });
 });
