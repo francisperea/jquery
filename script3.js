79136b98838d8e2e3a521d2170b4d4247574b2ff
Francis Perea <francis@francisperea.org>
2021-03-09 16:14:11 +0000

    Última versión con eventos

--- script3.js
+++ script3.js
@@ -1,5 +1,11 @@
-$(document).ready(function() {
+$(document).ready(function () {
   $("button.siguiente").on("click", function (event) {
     $("h1").addClass("cabecera");
+  });
+  $("ul#lista li").mouseenter(function () {
+    $(this).toggleClass("seleccionado");
+  });
+  $("ul#lista li").mouseleave(function () {
+    $(this).toggleClass("seleccionado");
   });
 });
